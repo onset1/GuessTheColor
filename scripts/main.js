c708598d5a9a0dc6ca6d85c8413623591719e262
onset1 <learningtoomuch@protonmail.com>
2022-08-13 12:43:38 -0300

    Mainly for settings

--- scripts/main.js
+++ scripts/main.js
@@ -88,9 +88,125 @@ function square_click_6() {
     };
 };
     
+// Options: adding event listeners
 
+document.getElementById("language").addEventListener("click", language_click);
 
+function language_click() {
+    document.getElementById("language").style.display = "none";
+    document.getElementById("difficulty").style.display = "none";
+    document.getElementById("visuals").style.display = "none";
+    document.getElementById("sounds").style.display = "none";
+    document.getElementById("color-system").style.display = "none";
+    var my_setting_language = document.getElementsByClassName("setting-language");
+    for (var i = 0; i<my_setting_language.length; i++) {
+        var element = my_setting_language.item(i);
+        element.style.display = "block";
+    }
+    document.getElementById("aside-chart-bottom").style.display = "flex";
+}
 
+document.getElementById("difficulty").addEventListener("click", difficulty_click);
+
+function difficulty_click() {
+    document.getElementById("language").style.display = "none";
+    document.getElementById("difficulty").style.display = "none";
+    document.getElementById("visuals").style.display = "none";
+    document.getElementById("sounds").style.display = "none";
+    document.getElementById("color-system").style.display = "none";
+    var my_setting_difficulty = document.getElementsByClassName("setting-difficulty");
+    for (var i = 0; i<my_setting_difficulty.length; i++) {
+        var element = my_setting_difficulty.item(i);
+        element.style.display = "block";
+    }
+    document.getElementById("aside-chart-bottom").style.display = "flex";
+}
+
+document.getElementById("visuals").addEventListener("click", visuals_click);
+
+function visuals_click() {
+    document.getElementById("language").style.display = "none";
+    document.getElementById("difficulty").style.display = "none";
+    document.getElementById("visuals").style.display = "none";
+    document.getElementById("sounds").style.display = "none";
+    document.getElementById("color-system").style.display = "none";
+    var my_setting_visuals = document.getElementsByClassName("setting-visuals");
+    for (var i = 0; i<my_setting_visuals.length; i++) {
+        var element = my_setting_visuals.item(i);
+        element.style.display = "block";
+    }
+    document.getElementById("aside-chart-bottom").style.display = "flex";
+}
+
+document.getElementById("sounds").addEventListener("click", sounds_click);
+
+function sounds_click() {
+    document.getElementById("language").style.display = "none";
+    document.getElementById("difficulty").style.display = "none";
+    document.getElementById("visuals").style.display = "none";
+    document.getElementById("sounds").style.display = "none";
+    document.getElementById("color-system").style.display = "none";
+    var my_setting_sounds = document.getElementsByClassName("setting-sounds");
+    for (var i = 0; i<my_setting_sounds.length; i++) {
+        var element = my_setting_sounds.item(i);
+        element.style.display = "block";
+    }
+    document.getElementById("aside-chart-bottom").style.display = "flex";
+}
+
+document.getElementById("color-system").addEventListener("click", colors_click);
+
+function colors_click() {
+    document.getElementById("language").style.display = "none";
+    document.getElementById("difficulty").style.display = "none";
+    document.getElementById("visuals").style.display = "none";
+    document.getElementById("sounds").style.display = "none";
+    document.getElementById("color-system").style.display = "none";
+    var my_setting_color = document.getElementsByClassName("setting-color");
+    for (var i = 0; i<my_setting_color.length; i++) {
+        var element = my_setting_color.item(i);
+        element.style.display = "block";
+    }
+    document.getElementById("aside-chart-bottom").style.display = "flex";
+}
+
+document.getElementById("button-back").addEventListener("click", back_click);
+
+function back_click() {
+    document.getElementById("language").style.display = "block";
+    document.getElementById("difficulty").style.display = "block";
+    document.getElementById("visuals").style.display = "block";
+    document.getElementById("sounds").style.display = "block";
+    document.getElementById("color-system").style.display = "block";
+    // Instead of writing 5 different functions...
+    var my_setting_language = document.getElementsByClassName("setting-language");
+    for (var i = 0; i<my_setting_language.length; i++) {
+        var element = my_setting_language.item(i);
+        element.style.display = "none";
+    }
+    var my_setting_difficulty = document.getElementsByClassName("setting-difficulty");
+    for (var i = 0; i<my_setting_difficulty.length; i++) {
+        var element = my_setting_difficulty.item(i);
+        element.style.display = "none";
+    }
+    var my_setting_visuals = document.getElementsByClassName("setting-visuals");
+    for (var i = 0; i<my_setting_visuals.length; i++) {
+        var element = my_setting_visuals.item(i);
+        element.style.display = "none";
+    }
+    var my_setting_sounds = document.getElementsByClassName("setting-sounds");
+    for (var i = 0; i<my_setting_sounds.length; i++) {
+        var element = my_setting_sounds.item(i);
+        element.style.display = "none";
+    }
+    var my_setting_color = document.getElementsByClassName("setting-color");
+    for (var i = 0; i<my_setting_color.length; i++) {
+        var element = my_setting_color.item(i);
+        element.style.display = "none";
+    }
+
+    document.getElementById("aside-chart-bottom").style.display = "none";
+}
 
 
 
